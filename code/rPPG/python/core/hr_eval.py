0ae49b691497a80a537468a1f09041c086a1f0d6
Yousuf <youmed.tech@gmail.com>
2020-03-18 21:24:20 +0000

    working on implementation

--- code/rPPG/python/core/hr_eval.py
+++ code/rPPG/python/core/hr_eval.py
@@ -2,10 +2,11 @@ from biosppy import storage
 from biosppy.signals import ecg as ECG
 import pyedflib
 import numpy as np
+from pipeline import tracking_pipeline
+from numpy import genfromtxt
 
-
-def get_signal(file_name):
-    f = pyedflib.EdfReader(file_name)
+def get_ecg_signal(file_path):
+    f = pyedflib.EdfReader(file_path)
     signal_labels = f.getSignalLabels()
     ecg = signal_labels.index("ECG")
     sampling_freq = f.getSampleFrequency(ecg)
@@ -14,25 +15,21 @@ def get_signal(file_name):
     f._close()
     return (ecg, sampling_freq)
 
+def get_ppg_signal(file_path):
+    return genfromtxt(file_path, delimiter=',')
+
 def mean_heart_rate(signal, sampling_freq):
     time_axis, filtered, rpeaks, template_time_axis, templates, heart_rate_time_axis, heart_rate = ECG.ecg(signal=signal, sampling_rate=sampling_freq*1.0, show=False)
     avg_hr = 60*len(rpeaks)*sampling_freq/len(signal)
     return avg_hr
 
 def evaluate(ppg_file, ecg_file, video, configuration):
-    tracking_pipeline()
+    window_size, offset = config.window_size, config.offset
+    values,pred_heart_rates,_,_,_,_,_,_,_ = tracking_pipeline(video, configuration)
+    ecg, ecg_sf = get_ecg_signal(ecg_file)
+    ecg_ws, ecg_o = len(ecg)*window_size/len(values), len(ecg)*offset/len(values)
+    ecg_hr = mean_heart_rate(ecg,ecg_sf)
 
-def evaluate_hr(rppg_hr, ecg_hr, number_of_frames, window_size, step_size):
-    correct = []
-    for index, hr in enumerate(rppg_hr):
-        progress = window_size + (index*step_size)
-        progress = int(len(ecg_hr)*progress/number_of_frames)
-        start = int((len(ecg_hr)*index*step_size)/number_of_frames)
-        reference = ecg_hr[start:progress]
-        correct_ref = np.min(reference) <= hr and np.max(reference) >= hr
-        correct.append(correct_ref)
-    return correct
-
-
-
-
+    ppg = get_ppg_signal(ppg_file)
+    ppg_ws, ppg_o = len(ppg)*window_size/len(values), len(ppg)*offset/len(values)
+    ppg_hr = mean_heart_rate(ppg,ppg_sf)
--- code/rPPG/python/core/pipeline.py
+++ code/rPPG/python/core/pipeline.py
@@ -2,9 +2,10 @@ import time as Timing
 import numpy as np
 import cv2 as cv
 from visualisation import Visualiser
+from configuration import Configuration
 
 PATH = 'rPPG/'
-def tracking_pipeline(tracker, roi, aggregate_function, video_path, signal_processor, window_size = 1200 , offset = 60, display = False):
+def tracking_pipeline(video_path, config:Configuration, display = False):
     # Profiling
     total_start = Timing.time()
     time_read = 0
@@ -22,7 +23,7 @@ def tracking_pipeline(tracker, roi, aggregate_function, video_path, signal_proce
     frame_rate = int(cap.get(cv.CAP_PROP_FPS))
     visualiser = None
     if display:
-        visualiser = Visualiser(PATH, tracker, video_path, roi, signal_processor, cap)
+        visualiser = Visualiser(PATH, config.tracker, video_path, config.region_selector, config.signal_processor, cap)
     while(cap.isOpened()):
         start = Timing.time()
         ret, frame = cap.read()
@@ -36,7 +37,7 @@ def tracking_pipeline(tracker, roi, aggregate_function, video_path, signal_proce
         faces, cropped = None, None
         while(not(face_found)):
             start = Timing.time()
-            faces, frame, cropped, profiling = tracker.track(frame)
+            faces, frame, cropped, profiling = config.tracker.track(frame)
             time_tracking += (Timing.time() - start)
             
             
@@ -53,15 +54,13 @@ def tracking_pipeline(tracker, roi, aggregate_function, video_path, signal_proce
         x,y,w,h = faces[0]
         
         start = Timing.time()
-#         print_heatmaps(frame, cropped)
-#         return
-        area_of_interest = roi.detect(cropped)
+        area_of_interest = config.region_selector.detect(cropped)
         time_roi += (Timing.time()-start)
         
         area_of_interest = np.pad(area_of_interest, ((y,height-(y+h)),(x,width-(x+w))), 'constant', constant_values=1)
         area_of_interest = np.repeat(area_of_interest[:, :, np.newaxis], 3, axis=2)
         frame = ma.masked_array(frame, mask=area_of_interest)
-        value = aggregate_function(frame)
+        value = config.aggregate_function(frame)
         values.append(value)
         if display:
             
@@ -71,12 +70,12 @@ def tracking_pipeline(tracker, roi, aggregate_function, video_path, signal_proce
             
         start = Timing.time()
         # if this is the first time we've reached the required number of values
-        if frame_number % window_size == 0 and frame_number//window_size == 1:
-            heart_rates.append(signal_processor(np.array(values[:window_size]), frame_rate))
-        elif (frame_number-window_size)%offset == 0 and frame_number>window_size:
-            n = int((frame_number-window_size)/offset)
-            print(f"N: {n}, Frame number: {frame_number}" )
-            heart_rates.append(signal_processor(np.array(values[n*offset : (n*offset)+window_size]), frame_rate))
+        if frame_number % config.window_size == 0 and frame_number//config.window_size == 1:
+            heart_rates.append(config.signal_processor(np.array(values[:config.window_size]), frame_rate))
+        elif (frame_number-config.window_size)%config.offset == 0 and frame_number>config.window_size:
+            n = int((frame_number-config.window_size)/config.offset)
+            print(f"N: {n}, Frame number: {frame_number}")
+            heart_rates.append(config.signal_processor(np.array(values[n*config.offset : (n*config.offset)+config.window_size]), frame_rate))
         time_ica += (Timing.time()-start)
     print(f"Total times: tracking {time_tracking}s, ROI {time_roi}s, display {time_display}s, ICA {time_ica}s, read {time_read}")
     print(f"Average per frame: tracking {time_tracking/frame_number}s, ROI {time_roi/frame_number}s, display {time_display/frame_number}s, ICA {time_ica/frame_number}s, reading video {time_read/frame_number}")
