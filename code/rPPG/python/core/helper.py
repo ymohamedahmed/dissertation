9d2de91b61ad57b0cb1037d5d36322c3393ff99e
Yousuf Mohamed-Ahmed <youmed.tech@gmail.com>
2020-04-08 20:40:39 +0100

    finished draft

--- code/rPPG/python/core/helper.py
+++ code/rPPG/python/core/helper.py
@@ -10,6 +10,9 @@ from configuration import PATH
 def get_test_images():
     return [cv.imread(file, cv.IMREAD_UNCHANGED) for file in glob.glob(f"{PATH}test-roi-images/*.jpg")]
 
+def get_eval_images():
+    return [cv.imread(file, cv.IMREAD_UNCHANGED) for file in glob.glob(f"{PATH}test-roi-images/*.jpeg")]
+
 def get_cropped_test_images():
     images = []
     for i in get_test_images():
@@ -19,16 +22,24 @@ def get_cropped_test_images():
 
 def display_heatmap(images):
     cols = 1
-    fig = plt.figure()
-    cbar_ax = fig.add_axes([.91, .3, .03, .4])
+    # fig = plt.figure()
+    # cbar_ax = fig.add_axes([.91, .3, .03, .4])
+    sns.set(font_scale=3)
     for n, image in enumerate(images):
-        a = fig.add_subplot(cols, np.ceil(len(images)/float(cols)), n + 1, aspect="equal")
+
+        # a = fig.add_subplot(cols, np.ceil(len(images)/float(cols)), n + 1, aspect="equal")
+        fig, ax = plt.subplots(figsize=(10,10))
 #         plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))
         sns.set_style("white")
-        ax = sns.heatmap(image, ax=a, xticklabels=[], yticklabels=[], cbar=not(n), cbar_ax = None if n else cbar_ax, cbar_kws={'label': 'Probability of being a skin pixel'})
-        plt.axis("off")
-    fig.set_size_inches(1.05*np.array(fig.get_size_inches()) * len(images))
-    fig.tight_layout(rect=[0, 0, .9, 1])
+        # ax = sns.heatmap(image, ax=a, xticklabels=[], yticklabels=[], cbar=not(n), cbar_ax = None if n else cbar_ax, cbar_kws={'label': 'Probability of being a skin pixel', 'shrink':1})
+        ax = sns.heatmap(image, ax=ax, xticklabels=[], yticklabels=[], cbar_kws={'label': 'Probability of being a skin pixel', 'shrink':1})
+        # plt.axis("off")
+    # fig.set_size_inches(1.2*np.array(fig.get_size_inches()) * len(images))
+    # fig.tight_layout(rect=[0, 0, .9, 1])
+    fig.tight_layout()
+    h,w = images[0].shape
+    impl_path = "/Users/yousuf/Workspace/dissertation/report/implementation/"
+    plt.savefig(f"{impl_path}hmap-{h}-{w}.png", bbox_inches="tight", pad_inches=0)
     plt.show()
 
 def show_images_plt(images):
